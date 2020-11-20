74f67b9b8a2816ee4bc4dbf391352f92299fbb79
Branden Kim <brandenk0@gmail.com>
2020-11-19 22:27:31 -0500

    Add base project

---
+++ b.reddit/b.reddit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  b.reddit
+//
+//  Created by Branden Kim on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
