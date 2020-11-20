608c67de4fbe9e7e52e213ff8ba69e1539eba9aa
Branden Kim <brandenk0@gmail.com>
2020-11-19 23:23:54 -0500

    Create three main targets for modules

---
+++ b.reddit/Posts/Posts.h
@@ -0,0 +1,18 @@
+//
+//  Posts.h
+//  Posts
+//
+//  Created by Branden Kim on 11/19/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Posts.
+FOUNDATION_EXPORT double PostsVersionNumber;
+
+//! Project version string for Posts.
+FOUNDATION_EXPORT const unsigned char PostsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Posts/PublicHeader.h>
+
+
